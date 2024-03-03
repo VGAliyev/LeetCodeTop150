c68fbb73366a6d0f50a1328a08f47d6a5563a572
VGAliyev <v.aliev84@yandex.ru>
2024-03-03 19:20:31 +0400

    Remove element complete

---
+++ src/RemoveElement.java
@@ -0,0 +1,30 @@
+public class RemoveElement {
+    // https://leetcode.com/problems/remove-element/submissions/1192730550?envType=study-plan-v2&envId=top-interview-150
+    public int removeElement(int[] numbers, int val) {
+        int numsEqValCount = numbers.length;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == val) {
+                numsEqValCount--;
+                numbers[i] = findNextNotVal(numbers, i, val);
+            }
+        }
+        for (int i = 0; i < numsEqValCount; i++) {
+            if(numbers[i] == -1) {
+                numbers[i] = findNextNotVal(numbers, i, val);
+            }
+        }
+        return numsEqValCount;
+    }
+
+    private int findNextNotVal(int[] numbers, int index, int val) {
+        int result = -1;
+        for (int i = index; i < numbers.length; i++) {
+            if(numbers[i] != val && numbers[i] != -1) {
+                result = numbers[i];
+                numbers[i] = -1;
+                return result;
+            }
+        }
+        return result;
+    }
+}
